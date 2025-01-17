42e28566cb424d390ef0b5e124f103729f258821
Eugene Hong <awidesky@naver.com>
2025-01-17 18:16:22 +0900

    add findFromPath

--- src/main/java/io/github/awidesky/ytdllp4j/util/ExecutablePathFinder.java
+++ src/main/java/io/github/awidesky/ytdllp4j/util/ExecutablePathFinder.java
@@ -5,18 +5,37 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class ExecutablePathFinder {
 
-	public static String findFromPath(String... commands) {
-		return findFromPath(Arrays.asList(commands));
+	public static String findFromPath(String executable, String... commands) {
+		return findFromPath(executable, Arrays.asList(commands));
 	}
 	
-	public static String findFromPath(List<String> commands) {
-		return null; //TODO
+	 //TODO add test
+	public static String findFromPath(String executable, List<String> options) {
+		for(String path : path()) {
+			try {
+				executable = new File(path, executable).getCanonicalPath();
+				LinkedList<String> command = new LinkedList<>();
+				command.add(executable);
+				command.addAll(options);
+				
+				ProcessBuilder pb = new ProcessBuilder(command);
+				Process p = pb.directory(new File(System.getProperty("user.home"))).start();
+				
+				if(p.waitFor() == 0)
+					return executable;
+			} catch (IOException | InterruptedException e) {
+				// TODO ignore?
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 	
 	public static String[] path() {
@@ -24,14 +43,15 @@ public class ExecutablePathFinder {
 		if(OSUtil.isUnix() && System.getenv("SHELL") != null) {
 			ProcessBuilder pb = new ProcessBuilder(System.getenv("SHELL"), "-c", "echo $PATH");
 			try {
-				Process p = pb.directory(null).start();
+				Process p = pb.directory(new File(System.getProperty("user.home"))).start();
 				if(p.waitFor() == 0) {
-					BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-					ret += File.pathSeparator + br.lines().collect(Collectors.joining(File.pathSeparator));
+					try (BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+						ret += File.pathSeparator + br.lines().collect(Collectors.joining(File.pathSeparator));
+					}
 				}
 			} catch (IOException | InterruptedException e) {
 				e.printStackTrace();
-			}		
+			}
 		}
 		
 		return Arrays.stream(ret.split(Pattern.quote(File.pathSeparator))).distinct().toArray(String[]::new);
