7b3fe72abac0ea7d8c08193a37ce14caf4e357bc
Eugene Hong <awidesky@naver.com>
2025-01-23 18:56:30 +0900

    put System print consumer in OutputConsumer interface

--- src/main/java/io/github/awidesky/ytdllp4j/outputConsumer/OutputConsumer.java
+++ src/main/java/io/github/awidesky/ytdllp4j/outputConsumer/OutputConsumer.java
@@ -2,6 +2,10 @@ package io.github.awidesky.ytdllp4j.outputConsumer;
 
 import java.util.function.Consumer;
 
+@FunctionalInterface
 public interface OutputConsumer extends Consumer<String> {
 	
+	public static final OutputConsumer redirectToSystemout = System.out::println;
+	public static final OutputConsumer redirectToSystemerr = System.err::println;
+
 }
