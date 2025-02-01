177a96f23eae76a5b4ecf5ec74f4b44d34c56650
Eugene Hong <awidesky@naver.com>
2025-02-01 16:42:34 +0900

    add variousCommandsTest

--- src/test/java/io/github/awidesky/ytdllp4j/YtdlpTest.java
+++ src/test/java/io/github/awidesky/ytdllp4j/YtdlpTest.java
@@ -4,10 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.io.IOException;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import io.github.awidesky.ytdllp4j.util.ExecutablePathFinder;
 import io.github.awidesky.ytdllp4j.util.YtdlpResultPrinter;
@@ -58,6 +66,38 @@ class YtdlpTest {
 		YtdlpResult res = ytdlp.update();
 		System.out.println("[updateTest] Process end");
 		YtdlpResultPrinter.print(res, "[updateTest]");
+	}
+	
+
+	@ParameterizedTest(name = "{0}")
+	@MethodSource("variousCommands")
+	void variousCommandsTest(String commandStr, Consumer<YtdlpCommand> addOption, String expectedOutput) throws IOException, InterruptedException {
+		YtdlpCommand command = new YtdlpCommand(link);
+		addOption.accept(command);
+		assertEquals(commandStr, command.toString());
+		String prefix = "[" + command.toString() + "]";
+		
+		System.out.println(prefix + " Process start");
+		YtdlpResult res = ytdlp.execute(command);
+		System.out.println(prefix + " Process end");
+		
+		YtdlpResultPrinter.print(res, prefix);
+		
+		if(expectedOutput != null) assertEquals(expectedOutput, res.getStdout().stream().collect(Collectors.joining("\n")));
+	}
+
+	static Stream<Arguments> variousCommands() throws Throwable {
+		return Stream.of(
+				Arguments.of("yt-dlp --get-filename -o %(title)s -- " + link,
+						(Consumer<YtdlpCommand>)c -> c.addOption("--get-filename").addOption("-o", "%(title)s"),
+						"Rick Astley - Never Gonna Give You Up (Official Music Video)"),
+				Arguments.of("yt-dlp -I 2 --get-filename -o %(title)s -- https://www.youtube.com/watch?v=C2xel6q0yao&list=PLlaN88a7y2_plecYoJxvRFTLHVbIVAOoc",
+						(Consumer<YtdlpCommand>)c -> c.addOption("-I", 2).addOption("--get-filename").addOption("-o", "%(title)s").setUrl("https://www.youtube.com/watch?v=C2xel6q0yao&list=PLlaN88a7y2_plecYoJxvRFTLHVbIVAOoc"),
+						"Rick Astley - Never Gonna Give You Up (Official Music Video)"),
+				Arguments.of("yt-dlp --simulate -- " + link,
+						(Consumer<YtdlpCommand>)c -> c.addOption("--simulate"),
+						null)
+	    );
 	}
 
 }
